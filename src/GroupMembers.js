871c5b707ffa592a6354d761a0b9f244d203101a
yadieldaniel <yadieldaniel2119@gmail.com>
2024-12-25 01:41:32 -0800

    my commit

--- src/GroupMembers.js
+++ src/GroupMembers.js
@@ -1,14 +1,14 @@
 import React from "react";
 
 const GroupMembers = () => {
-  // Array of group members
+  
   const members = ["a", "b", "c", "d", "e"];
 
   return (
     <div>
       <h3>Group Members</h3>
       <ul>
-        {/* Use .map() to iterate over the array and display each name */}
+  
         {members.map((member, index) => (
           <li key={index}>{member}</li>
         ))}
