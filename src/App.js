b96f187f0ec3b4dc9140a65ce53236b6994e91c8
yadieldaniel <yadieldaniel2119@gmail.com>
2024-12-31 12:38:40 -0800

    addcounter app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+
 import GroupMembers from "./GroupMembers";
 import UserCard from './UserCard';
+import React, { useState } from "react";
 
 function App() {
+   const [count, setCount] = useState(0);
   return (
     <div className="App">
       <h1>welcome to this page</h1>
-      <GroupMembers/>
-       <h1>User card </h1>
-     
-      <UserCard name = "yadiel" age ={20} email= "yadieldaniel2119@gmail.com"/>
-     
+      <GroupMembers />
+      <h1>User card </h1>
+
+      <UserCard name="yadiel" age={20} email="yadieldaniel2119@gmail.com" />
+      <h1>Counter Application</h1>
+      <h2>Count: {count}</h2>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+      <button onClick={() => setCount(0)}>Reset</button>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -27,7 +34,6 @@ function App() {
           Learn React
         </a>
       </header>
-     
     </div>
   );
 }
